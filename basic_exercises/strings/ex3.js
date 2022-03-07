bbc863a23a69002ac7b0572fd1c17e000421f6c1
Cortland Young <dcy.y@outlook.com>
2022-03-07 12:29:13 -0500

    strings exercises

---
+++ basic_exercises/strings/ex3.js
@@ -0,0 +1,8 @@
+function repeat(num, string) {
+  for (let count = 1; count <= num; count += 1){
+    console.log(string);
+  }
+}
+
+repeat(3, 'ha'); // 'hahaha'
+repeat(12, 'jajaj');
---
+++ basic_exercises/strings/ex6.js
@@ -0,0 +1,3 @@
+let byteSequence = 'TXkgaG92ZXJjcmFmdCBpcyBmdWxsIG9mIGVlbHMu';
+const char = 'x';
+console.log(`The character \"${char}\" ${byteSequence.includes('x') ? 'is' : 'is not'} included in the string.`);
